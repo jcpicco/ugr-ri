2a86d2a8c385e4e05f304750b233dc417c30cd30
Juan Manuel Consigliere Picco <jmconsigliere1998@gmail.com>
2021-10-17 20:49:38 +0200

    P2: Ejercicio 3 terminado

---
+++ p2/ej3/practica2_3.java
@@ -0,0 +1,57 @@
+import java.io.IOException;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
+import org.apache.lucene.analysis.core.SimpleAnalyzer;
+import org.apache.lucene.analysis.core.StopAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.custom.CustomAnalyzer;
+import org.apache.lucene.analysis.ngram.NGramFilterFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+import org.apache.tika.Tika;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import java.util.*;
+
+public class practica2_3{
+	public static void main(String[] args) throws IOException{
+		Tika tika = new Tika();
+        Metadata metadata = new Metadata();
+		File archivo = new File("../test/hamlet.txt");
+
+		String text = new String();
+
+		try{
+			text = tika.parseToString(archivo);
+		}catch (Exception e){ 
+			System.out.println("No se puede parsear...\n\n");
+		}
+
+        tika.parse(archivo,metadata); //Parseamos el fichero de texto plano
+        String nombre_archivo = archivo.getName();
+
+		Analyzer analyzer = CustomAnalyzer.builder()
+   			.withTokenizer("standard")
+   			.addTokenFilter("lowercase")
+   			.addTokenFilter("apostrophe")
+   			.addTokenFilter("nGram","minGramSize","2","maxGramSize","2")
+   			.build();
+
+		TokenStream stream = analyzer.tokenStream(null, text);
+
+		stream.reset(); //Se le llama antes de usar incrementToken()
+		while(stream.incrementToken())
+		    System.out.print(" || "+stream.getAttribute(CharTermAttribute.class).toString());
+
+		System.out.println();
+			
+		stream.end(); //Se le llama cuando se termina de iterar
+		stream.close(); //Liberas los recursos asociados al stream
+	}
+}
