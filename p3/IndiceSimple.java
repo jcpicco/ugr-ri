e41c6f1d61bbc52ae99d7e6ae97e504b5c8ea186
Juan Manuel Consigliere Picco <jmconsigliere1998@gmail.com>
2021-11-03 10:54:29 +0100

    P3: Falta resolver link

--- p3/IndiceSimple.java
+++ p3/IndiceSimple.java
@@ -4,49 +4,101 @@ import java.io.FileReader;
 import com.opencsv.exceptions.CsvException;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.document.*;
+import org.apache.lucene.search.similarities.Similarity;
+import org.apache.lucene.search.similarities.ClassicSimilarity;
+import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
+import org.apache.lucene.analysis.core.SimpleAnalyzer;
+import org.apache.lucene.analysis.core.StopAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.core.KeywordAnalyzer;
+import org.apache.lucene.analysis.en.EnglishAnalyzer;
+import org.apache.lucene.analysis.miscellaneous.PerFieldAnalyzerWrapper;
+import java.nio.file.Paths;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.io.File;
+import java.io.FileInputStream;
 
-public class IndiceSimple {
+public class indiceSimple {
     String indexPath = "./index";
-    String docPath = "./DataSet";
+    String docPath = "./docs";
     boolean create = true;
     private IndexWriter writer;
+    Map<String, Analyzer> analyzerPerField = new HashMap<String, Analyzer>();
 
-    public void configurarIndice(Analyzer analyzer, Similarity similarity) throws IOException {
-        IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+    indiceSimple(){
+        analyzerPerField.put("author", new SimpleAnalyzer());
+        analyzerPerField.put("author_id", new StopAnalyzer(EnglishAnalyzer.ENGLISH_STOP_WORDS_SET));
+        analyzerPerField.put("title", new SimpleAnalyzer());
+        analyzerPerField.put("source_title", new SimpleAnalyzer());
+        analyzerPerField.put("doi", new KeywordAnalyzer());
+        analyzerPerField.put("link", new StandardAnalyzer());
+        analyzerPerField.put("affiliations", new SimpleAnalyzer());
+        analyzerPerField.put("abstract", new EnglishAnalyzer());
+        analyzerPerField.put("author_keywords", new SimpleAnalyzer());
+        analyzerPerField.put("index_keywords", new SimpleAnalyzer());
+        analyzerPerField.put("doc_type", new KeywordAnalyzer());
+        analyzerPerField.put("eid", new KeywordAnalyzer());
+    }
+
+    public void configurarIndice(Analyzer analyzer, Similarity similarity, String path) throws IOException {
+        PerFieldAnalyzerWrapper analyzerWrapper = new PerFieldAnalyzerWrapper(analyzer, analyzerPerField);
+        
+        IndexWriterConfig iwc = new IndexWriterConfig(analyzerWrapper);
         
         iwc.setSimilarity(similarity);
+        // iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
         iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
-        // iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
 
-        Directory dir = FSDirectory.open(Pathss.get(indexPath));
+        Directory dir = FSDirectory.open(Paths.get(indexPath+"/"+path));
         writer = new IndexWriter(dir, iwc);
     }
 
-    public void indexarDocumentos() {
-        CSVReader reader = new CSVReader(new FileReader("scopus1.csv"));
-        List<String[]> r = reader.readAll();
-        r.forEach(x -> System.out.println(Arrays.toString(x)));
-        Document doc = new Document();
-        Integer start = ...;
-        Integer end = ...;
-        String aux = cadena.substring(start, end);
-        Integer valor = Integer.decode(aux);
-
-        doc.add(new IntPoint("ID", valor));
-        doc.add(new StoreField("ID", valor));
-
-        String cuerpo = cadena.substring(start, end);
-
-        doc.add(new TextField("Body", cuerpo, Field.Store.YES));
-
-        writer.addDocument(doc);
+    public void indexarDocumentos(File archivo) throws IOException, CsvException {
+            CSVReader reader = new CSVReader(new FileReader(docPath+"/"+archivo.getName()));
+            reader.skip(1);
+            String[] r = reader.readNext();
+            
+            do{
+                Document doc = new Document();
+                doc.add(new TextField("author", r[0], Field.Store.YES));
+                doc.add(new TextField("author_id", r[1], Field.Store.YES));
+                doc.add(new TextField("title", r[2], Field.Store.YES));
+                doc.add(new IntPoint("year", Integer.parseInt(r[3])));
+                doc.add(new StoredField("year", Integer.parseInt(r[3])));
+                doc.add(new TextField("source_title", r[4], Field.Store.YES));
+                doc.add(new StoredField("volume", r[5]));
+                doc.add(new StoredField("issue", r[6]));
+                doc.add(new StoredField("article_number", r[7]));
+                doc.add(new StoredField("page_start", r[8]));
+                doc.add(new StoredField("page_end", r[9]));
+                doc.add(new StoredField("page_count", r[10]));
+                if(!r[11].equals("")){
+                    doc.add(new SortedNumericDocValuesField("cited_by", Long.parseLong(r[11])));
+                    doc.add(new StoredField("cited_by", Long.parseLong(r[11])));   
+                }
+                doc.add(new StringField("doi", r[12], Field.Store.YES));
+                doc.add(new StringField("link", r[13], Field.Store.YES));
+                doc.add(new TextField("affiliations", r[14], Field.Store.YES));
+                doc.add(new TextField("abstract", r[16], Field.Store.YES));
+                doc.add(new TextField("author_keywords", r[17], Field.Store.NO));
+                doc.add(new TextField("index_keywords", r[18], Field.Store.NO));
+                doc.add(new StringField("doc_type", r[19], Field.Store.NO));
+                doc.add(new StoredField("public_status", r[20]));
+                doc.add(new StringField("eid", r[23], Field.Store.YES));
+                writer.addDocument(doc);
+                r = reader.readNext();
+            }while(r!=null);
     }
 
-    public void close() {
+    public void close() throws IOException, CsvException {
         try {
             writer.commit();
             writer.close();
@@ -55,15 +107,18 @@ public class IndiceSimple {
         }
     }
 
-    public static void main (String[] args) {
+    public static void main(String[] args) throws IOException, CsvException {
+        File dir = new File("./docs");
+        File[] archivos = dir.listFiles();
         Analyzer analyzer = new StandardAnalyzer();
         Similarity similarity = new ClassicSimilarity();
-        IndiceSimple baseline = new IndiceSimple( ... );
+        indiceSimple baseline = new indiceSimple();
 
-        baseline.configurarIndice(analyzer, similarity);
-        IndiceSimple.indexarDocumentos();
-        baseline.close();
-
+        for(File archivo : archivos){
+            baseline.configurarIndice(analyzer, similarity, archivo.getName().substring(0, archivo.getName().lastIndexOf(".")));
+            baseline.indexarDocumentos(archivo);
+            baseline.close();
+        }
     }
 }
 
